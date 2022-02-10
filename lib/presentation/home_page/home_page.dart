51f341dd8390e691d17383e6d60a897501d41319
doniyormurodkulov <donikmurodkulov98@gmail.com>
2022-02-10 14:28:11 +0500

    services done for templates and decoration elements

--- lib/presentation/home_page/home_page.dart
+++ lib/presentation/home_page/home_page.dart
@@ -1,35 +1,35 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
-// import 'package:photo_album/presentation/profile/profile_page.dart';
-// import 'package:titled_navigation_bar/titled_navigation_bar.dart';
-//
-// class HomePage extends StatefulWidget {
-//   const HomePage({Key? key}) : super(key: key);
-//
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-//
-// class _HomePageState extends State<HomePage> {
-//   int _pageIndex = 0;
-//
-//   List<Widget> _tabs = [
-//     Container(),
-//     Container(),
-//     ProfilePage(),
-//   ]
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: _tabs.elementAt(_pageIndex),
-//       bottomNavigationBar: TitledBottomNavigationBar(
-//         currentIndex: _pageIndex,
-//         onTap: (value) => setState(() => _pageIndex = value),
-//         items: [
-//           TitledNavigationBarItem(icon: SvgPicture.asset('assets/sgvs/home.svg'), title: title)
-//         ],
-//       ),
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:photo_album/presentation/profile/profile_page.dart';
+import 'package:titled_navigation_bar/titled_navigation_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _pageIndex = 0;
+
+  List<Widget> _tabs = [
+    Container(),
+    Container(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // body: _tabs.elementAt(_pageIndex),
+      // bottomNavigationBar: TitledBottomNavigationBar(
+      //   currentIndex: _pageIndex,
+      //   onTap: (value) => setState(() => _pageIndex = value),
+      //   items: [
+      //     TitledNavigationBarItem(icon: SvgPicture.asset('assets/sgvs/home.svg'), title: title)
+      //   ],
+      // ),
+    );
+  }
+}
